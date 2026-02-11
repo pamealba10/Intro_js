78a21af0e53713830af53be69c205fec87169080
Pamela <arcticpam1008@gmail.com>
2026-02-10 19:16:35 -0600

    new file

---
+++ tipos-de-dato.js
@@ -0,0 +1,57 @@
+/* EJERCICIO 1: Tipos de dato en JavaScript
+El objetivo es:  crear un repositorio local y que lo vincules con un repositorio en Github. En este repositorio, agrega los archivos javascript que necesites para los ejercicios de tu clase y los descritos a continuación.
+
+En este archivo agrega el código para imprimir en la consola los distintos tipos de datos que existen en JS. Como ejemplo tenemos: 'console.log(typeof 42);'. Esta intrucción debería imprimir en consola 'number' ya que el número 42 es del tipo 'number' en JavaScript.
+Agrega uno o varios ejemplos para los siguientes valores y que se imprima a qué tipo de dato pertenecen dichos valores, justo como en el ejemplo anterior. Los valores son:
+42
+'Veinticinco'
+-666
+true
+0
+''
+null
+undefined
+FALSE
+
+Agrega tus propios ejemplos y experimenta con algunos valores y sus tipos de datos.
+ */
+
+
+// TIPOS DE DATOS EN JAVASCRIPT
+
+// "Algo de codigo" /* Tambien se puede comentar así en JS 
+
+
+/* Strings
+Entre comlillas
+"Hola soy un string" 
+'Yo tambien soy string'
+
+Numbers
+2
+1.1
+-1
+
+//Booleans
+true 
+false
+
+// Null
+null
+
+// Undefined
+undefined */
+
+console.log(typeof "Esto es un string");
+console.log(typeof PAMELA);
+console.log(typeof "PAMELA");
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof '');
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+console.log(typeof -50);
